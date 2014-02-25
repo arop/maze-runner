189331092f805f2ff240fc3f7ef198c7ec3ed25f
André Pires <andre.r.o.pires@gmail.com>
2014-02-25 02:40:21 +0000

    with random S

--- src/Board.java
+++ src/Board.java
@@ -1,34 +1,41 @@
 
 public class Board {
-	private static String[][] maze ;
-	
-	
-	public String[][] getMaze() { 
-		return maze ;
-	}
+	private static String[][] Board;
 	
 	public Board(int n) {
-		maze = new String[n][n] ;
-		for( int i = 0 ; i < maze.length ; i++){
-			for (int k = 0 ; k < maze[i].length; k++){
-				maze[i][k] = "X" ;
+		Board = new String[n][n] ;
+		for( int i = 0 ; i < Board.length ; i++){
+			for (int k = 0 ; k < Board[i].length; k++){
+				Board[i][k] = "X" ;
 			}
 					
 		}
+	}
+	
+	
+	static void makePath(int n) {
+		int i=1; 
+		int j=1;
+		
+		while(i<n && j<n) {
+		int r=(int) Math.random();
+		
+		if(r==0) i++;
+		else if(r==1) j++;
+		}
+				
 	}
 	
 	static void showBoard() {
-		for( int i = 0 ; i < maze.length ; i++){
-			for (int k = 0 ; k < maze[i].length; k++){
-				System.out.print(maze[i][k]);
+		for( int i = 0 ; i < Board.length ; i++){
+			for (int k = 0 ; k < Board[i].length; k++){
+				System.out.print(Board[i][k]);
 				System.out.print(" ") ;
 			}
-			System.out.println() ;
+			System.out.println();
 					
 		}
 	}
-	
-	
 
 }
 	
