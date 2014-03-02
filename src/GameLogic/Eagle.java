61d4aa251c82762ee427126300227e127f89cdc7
ei12058 <ei12058@481db94f-f5f5-4b71-ab5e-8d926a57a53e>
2014-03-02 19:06:55 +0000

    c/ varios size do board

--- src/GameLogic/Eagle.java
+++ src/GameLogic/Eagle.java
@@ -5,34 +5,31 @@ public class Eagle extends Personagem {
 	private int dY = 0 ;
 	private int iX = 0 ;
 	private int iY = 0 ;
-	
-	
+
 	public Eagle(int x, int y) {
 		super(x,y);
 		iX = x ;
 		iY = y ;
 		symbol = "v";
 	}
-	
+
 	public int getdX() { return dX ; }
 	public int getdY() { return dY ; }
 	public int getiY() { return iY ; }
 	public int getiX() { return iX ; }
-	
+
 	public void setdX(int x) { dX = x; }
 	public void setdY(int y) { dY = y; }
-
 
 	public boolean nextPosition(mazeBuilder board, int x, int y) {
 		return true ;
 	}
-	
+
 	public void move(mazeBuilder b, String input) {
 		if (Y < dY) MoveRight(b) ;
 		if (Y > dY) MoveLeft(b) ;
 		if (X < dX) MoveDown(b) ;
-		if (X > dX) MoveUp(b);
-		
+		if (X > dX) MoveUp(b);		
 	}
 
 
--- src/GameLogic/Game.java
+++ src/GameLogic/Game.java
@@ -3,15 +3,41 @@ import java.util.Scanner;
 
 public class Game {
 
-
-	public static mazeBuilder b = new mazeBuilder(31);
-	public static Personagem h = new Heroi(b.getMaze());
-	public static Dragao d ;
-	public static Sword s = new Sword(b.getMaze());
-	public static Eagle eg = new Eagle (h.getX(),h.getY()) ;
-	public static boolean EagleReleased = false ;
+	public static mazeBuilder b;
+	public static Personagem h;
+	public static Dragao d;
+	public static Sword s;
+	public static Eagle eg;
+	public static boolean EagleReleased = false;
 
 	public static void main (String[] args) {
+		boolean validN=false;
+
+		Scanner size = new Scanner(System.in);
+
+		while(!validN) {
+			System.out.println("Please insert size of maze (odd number, over 7) (1 for default maze): ");
+			String n= size.nextLine();
+			int n1=Integer.parseInt(n);
+
+			if(n1==1) {
+				b= new mazeBuilder(n1); 
+				validN= true; 
+				h= new Heroi(1,1);
+				s= new Sword(8,1);
+			}
+
+			else if(n1>=7 && n1%2!=0) {
+				b=new mazeBuilder(n1); 
+				validN=true; 
+				h= new Heroi(b.getMaze());
+				s= new Sword(b.getMaze());
+			}
+			else  System.out.println("Not valid! Insert new one: ");
+		}
+
+		eg = new Eagle (h.getX(),h.getY());
+
 		do {
 			d = new Dragao(b.getMaze());
 		} while(endGame());
@@ -28,34 +54,31 @@ public class Game {
 			Scanner myScanner = new Scanner(System.in);
 			String input = myScanner.nextLine();
 			if(endGame()) {myScanner.close(); return; }
-			
+
 			if(input.equals("f")) {
 				EagleReleased = true ;
 				eg.setdX(s.getX()) ;
 				eg.setdY(s.getY());
 			}
-			
+
 			h.move(b,input);
-			
+
 			if(EagleReleased) eg.move(b, input);
 			else {
 				eg.setX(h.getX());
 				eg.setY(h.getY()) ;
 			}
-			
+
 			if(eg.getX() == s.getX() && eg.getY() == s.getY()) {
 				s.disable() ;
 				eg.setSymb("V") ;
 				eg.setdX(eg.getiX());
 				eg.setdY(eg.getiY());
 			}
-	
-
 
 			int x= (int) Math.round(Math.random());
 			if(x == 0) d.setSleeping();
-			if(!d.getSleeping())
-				d.move(b,input);
+			if(!d.getSleeping()) d.move(b,input);
 		} while(!won());
 
 	}
@@ -76,5 +99,4 @@ public class Game {
 			return true;
 		return false;
 	}
-
 }
