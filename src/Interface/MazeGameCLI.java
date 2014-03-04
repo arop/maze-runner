9b82d5ccd390ae7418d7a233130efcd53b820370
ei12068 <ei12068@481db94f-f5f5-4b71-ab5e-8d926a57a53e>
2014-03-04 19:14:33 +0000

    Interface update

--- src/Interface/MazeGameCLI.java
+++ src/Interface/MazeGameCLI.java
@@ -1,16 +1,85 @@
 package Interface;
 
-public class MazeGameCLI { 
+import java.util.Scanner;
 
-	public void MainMenu() {	
+import GameLogic.Game;
+
+public class MazeGameCLI { 
+	
+	public static Game g1;
+	
+	
+	public static void main (String[] args) {
+		mainMenu() ;
+		
+	}
+	
+	public static void mainMenu() {
 		System.out.println(">>> MAZE GAME <<<");
 		System.out.println() ;
 		System.out.println("1. Play Game");
 		System.out.println("2. Options") ;
 		System.out.println("3. Quit") ;
+		
+		Scanner option = new Scanner(System.in);
+		String n = option.nextLine();
+		int a = Integer.parseInt(n);
+		switch(a) {
+			case 1:
+				g1 = new Game() ;
+				break;
+			case 2: 
+				optionsMenu() ;
+				break;
+			case 3:
+				return ;
+		}
+		
+		g1.Play();
+
 	}
+
+	private static void optionsMenu() {
+		System.out.println("> OPTIONS <");
+		System.out.println() ;
+		System.out.println("1. Random maze, choose size");
+		System.out.println("2. Multiple dragons") ;
+		System.out.println("3. Sleeping dragons") ;
+		System.out.println("4. Cancel") ;
+		
+		Scanner option = new Scanner(System.in);
+		String b = option.nextLine();
+		int a = Integer.parseInt(b);
+		
+		switch(a) {
+		case 1:
+			boolean validN=false;
+
+			Scanner size = new Scanner(System.in);
+
+			while(!validN) {
+				System.out.println("Please insert size of maze (odd number, over 7) (1 for default maze): ");
+				String n= size.nextLine();
+				int n1=Integer.parseInt(n);
+
+				if( n1>=7 && n1%2!=0){
+					validN= true; 
+					g1 = new Game(n1);
+					break;
+				}
+
+				else  System.out.println("Not valid! Insert new one: ");
+			}
+			break;
+			
+		case 2:
+		
+		case 4: 
+		
+	}
+		
 	
 
 
 
-}
+}}
