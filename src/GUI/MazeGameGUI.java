6d0292ad041fe709e2a7408e382e005427bac222
André Pires <andre.r.o.pires@gmail.com>
2014-03-19 20:01:11 +0000

    c/ menus

--- src/GUI/MazeGameGUI.java
+++ src/GUI/MazeGameGUI.java
@@ -1,24 +1,15 @@
 package GUI;
-import javax.swing.*; 
-
-import java.awt.*;
-import java.awt.FlowLayout;
 
 import GameLogic.Game;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-
 
 public class MazeGameGUI  {
 	
 	public static Game g1 = new Game();
-	
+	public int WIDTH = 500;
+	public int HEIGHT = 300;
 	
 	public static void main(String[] args) {
-		MainMenu coiso = new MainMenu();
-		
-		
+		new MainMenu();	
 	}
 	
 	
--- src/GUI/OptionsMenu.java
+++ src/GUI/OptionsMenu.java
@@ -4,10 +4,11 @@ import java.awt.*;
 
 import javax.swing.*;
 
+import GameLogic.Game;
+
 import java.awt.event.*;
 
-
-public class OptionsMenu {
+public class OptionsMenu extends MazeGameGUI implements Menu {
 
 	private JFrame frame;
 	private JButton back_button;
@@ -15,9 +16,11 @@ public class OptionsMenu {
 	private JLabel sizeLabel;
 	private JTextField numDragonsField;
 	private JLabel numDragonsLabel;
-	private JLabel movingDragonsLabel;
-	private JLabel sleepingDragonsLabel;
+	private JCheckBox movingOption;
+	private JCheckBox sleepingOption;
 
+	private boolean validSize = true;
+	private boolean validNumDragons = true;
 
 	public OptionsMenu() {
 		frame = new JFrame("Options Menu");
@@ -25,55 +28,98 @@ public class OptionsMenu {
 		frame.getContentPane().setLayout(new GridLayout(5,5));
 		createWidgets();
 		addWidgets(frame.getContentPane());
+		
+		frame.setPreferredSize(new Dimension(WIDTH,HEIGHT)); //change values
+		
 		frame.pack();
 		frame.setVisible(true);
-
-
 	}
 
-	private void createWidgets(){ 
+	public void createWidgets(){ 
 		back_button = new JButton("Back");
 		back_button.addActionListener(new BackListener());
 		sizeField = new JTextField();
-		sizeLabel = new JLabel("Size");
+		sizeLabel = new JLabel("Size of maze");
 		numDragonsField  = new JTextField();
 		numDragonsLabel = new JLabel("Number of dragons");
-		movingDragonsLabel = new JLabel("Moving dragons:");
-		sleepingDragonsLabel = new JLabel("Sleeping dragons:");
-		
-		
-		
+		movingOption = new JCheckBox("Moving dragons");
+		sleepingOption = new JCheckBox("Sleeping dragons");
+
+		sizeField.addActionListener(new NumberListener());
+		numDragonsField.addActionListener(new NumberListener());
+		movingOption.addItemListener(new OptionsListener());
+		sleepingOption.addItemListener(new OptionsListener());
 	}
 
 
-	private void addWidgets (Container cont) {
-
-		cont.add(sizeField);
+	public void addWidgets (Container cont) {
 		cont.add(sizeLabel);
-		cont.add(numDragonsField);
+		cont.add(sizeField);		
 		cont.add(numDragonsLabel);
-		cont.add(movingDragonsLabel);
-		cont.add(sleepingDragonsLabel);
+		cont.add(numDragonsField);
+		cont.add(movingOption);
+		cont.add(sleepingOption);
 		cont.add(back_button);
 	}
 
-
-
 	public class BackListener implements ActionListener {
-
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
+			//TODO this
+			//if(!validSize) pop up size not changed
+			//if(!validNumDragons) pop up
 			frame.setVisible(false);
-			return ;
-
+			g1.setBoard();
+			new MainMenu();
 		}
 	}
 
+	public class NumberListener implements ActionListener {
 
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			String s;
+			if(arg0.getSource()==sizeField) {
+				s = sizeField.getText();
+				int n1=Integer.parseInt(s);
 
+				if( n1>=5 && n1%2!=0){
+					validSize= true;
+					g1 = new Game(n1+2);
+				}
 
+				else  validSize=false;
+			}
 
+			if(arg0.getSource()==numDragonsField) {
+				s= numDragonsField.getText();
+				int n1=Integer.parseInt(s);
 
+				if(n1<(g1.getSize()/7)) {
+					validNumDragons=true;
+					g1.setNumber_dragons(n1);
+				}
+				else validNumDragons=false;				
+			}
+
+		}
+
+	}
+
+	public class OptionsListener implements ItemListener {
+
+		@Override
+		public void itemStateChanged(ItemEvent arg0) {
+			if(arg0.getSource()==movingOption) { 
+				g1.setMovingDragons();
+			}
+			if(arg0.getSource()==sleepingOption) { 
+				g1.setSleepingDragons();
+			}
+		}
+
+	}
 }
+
 
 
