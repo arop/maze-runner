a77408df92ec931273bb95a9198d990087a70926
André Pires <andre.r.o.pires@gmail.com>
2014-03-25 19:02:04 +0000

    a fazer display primitivo do board

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -2,8 +2,6 @@ package GUI;
 
 import java.awt.Container;
 
-import GameLogic.Game;
-
 public interface Menu {
 	void createWidgets();
 	void addWidgets (Container cont); 
--- src/GUI/MenuPlay.java
+++ src/GUI/MenuPlay.java
@@ -9,24 +9,39 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-import GUI.MainMenu.MainMenuListener;
+import javax.swing.JTextArea;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 public class MenuPlay extends MazeGameGUI implements Menu {
 	private JFrame frame;
 	private JButton back_button;
 	private JButton quit_button;
+	private String b=g1.getBoard().toString();//.replace("\n", "<br/>"); //not working
+	private final JTextArea textArea = new JTextArea(b, g1.getSize(), g1.getSize());
+	private JPanel panel;
 	
 	
 	public MenuPlay() {
 		frame = new JFrame("Menu Play");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(3,3));
+		
+		panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Maze", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		frame.getContentPane().add(panel);
+		panel.add(textArea);
+		textArea.setEditable(false);
+		textArea.setVisible(true);
+		
 		createWidgets();
 		addWidgets(frame.getContentPane());
+		
 		
 		frame.setPreferredSize(new Dimension(WIDTH,HEIGHT)); //change values
 		
 		frame.pack();
+		
 		frame.setVisible(true);
 	}
 	
