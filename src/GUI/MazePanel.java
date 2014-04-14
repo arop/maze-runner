c0497dd2759d41211f2f8a1eb97fd06f42f9e8a4
ei12068 <ei12068@481db94f-f5f5-4b71-ab5e-8d926a57a53e>
2014-04-14 11:55:41 +0000

    MazePanel without labels. Resizable

--- src/GUI/MazePanel.java
+++ src/GUI/MazePanel.java
@@ -1,72 +1,71 @@
 package GUI;
 
+
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.print.DocFlavor.URL;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 import javax.swing.border.EmptyBorder;
 
+import GameLogic.Board;
 import GameLogic.Game;
 
-public class MazePanel extends JLabel {
+public class MazePanel extends JPanel {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 7836117138272018391L;
-	private JLabel GraphicBoard[][];
+	
 	private Game g1;
 	private MazeGameGUI frame;
-	private int realSize = 10;
-	private ImageIcon icon;
-	private ImageIcon ground;
-	private ImageIcon hero;
-
+	private int realSize;
+	
+	private static final long serialVersionUID = 7836117138272018391L;
+	
+	private BufferedImage sword = creatImage("imagens/sword.jpg");
+	private BufferedImage Wall =  creatImage("imagens/muralhaTemp.jpg");
+	private BufferedImage Water =  creatImage("imagens/Water.gif");
+	private BufferedImage Hero =  creatImage("imagens/hero.jpg");
+	private BufferedImage Hero_sword =  creatImage("imagens/HeroSword.jpg");
+	private BufferedImage Dragon_awake  =  creatImage("imagens/dragonAwake.png");
+	private BufferedImage Dragon_sleeping = creatImage("images/DragonSleeping.jpg");
+	private BufferedImage Eagle  =  creatImage("imagens/eagle.jpg");
 
 	public MazePanel(Game currentGame, MazeGameGUI window){
-
 		g1 = currentGame;
 		frame = window;
-
-		icon = new ImageIcon("Water.gif");
-		ground = new ImageIcon("ground.jpg");
-		hero = new ImageIcon("Hero.jpg");
-
-		this.setIcon(icon);
-
-
-
+		
 		if(g1.getSize()-2 < 5) realSize = 10;
 		else realSize = g1.getSize()-2;
-
-		GraphicBoard = new JLabel[realSize][realSize];
-
-		this.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this.setLayout(new GridLayout(realSize,realSize));
-
-
-		for (int i = 0 ; i < realSize; i++) {
-			for (int j = 0; j < realSize; j++) {
-				GraphicBoard[i][j] = new JLabel() ;
-				GraphicBoard[i][j].setOpaque(true);
-				this.add(GraphicBoard[i][j]);
-
-			}
-		}
-
+		
 		Play();
 		UpdateGraphicBoard();
+		
 	}
 
-	public void Play() {
-		KeyListener listener = new MyKeyListener();
-		this.addKeyListener(listener);
+	private BufferedImage creatImage(String path) {
+		BufferedImage image = null;
+		try {                
+			image = ImageIO.read(new File(path));
+		} catch (IOException ex) {
+			// handle exception...
+		}
+		return image;
 	}
-
+	
 	public class MyKeyListener implements KeyListener {
 		private int output ;
 		@Override
@@ -94,8 +93,7 @@ public class MazePanel extends JLabel {
 			}
 
 			UpdateGraphicBoard();
-			
-		
+				
 			
 			switch(output) {
 			case 1: //quit
@@ -117,6 +115,8 @@ public class MazePanel extends JLabel {
 			}
 
 		}
+		
+	
 		@Override
 		public void keyReleased(KeyEvent e) {
 			// TODO Auto-generated method stub
@@ -128,43 +128,64 @@ public class MazePanel extends JLabel {
 
 		}
 	}
+	
 
-	//TODO change color of specials (armado, sleeping dragons, etc)
-	public void UpdateGraphicBoard() {
-		for (int i = 0 ; i < realSize; i++) {
-			for (int j = 0; j < realSize; j++) {
+	public void Play() {
+		KeyListener listener = new MyKeyListener();
+		this.addKeyListener(listener);
+	}
 
-				if (g1.getBoard().getCurrentState()[i][j].equals(" ")) {
-					GraphicBoard[i][j].setIcon(ground);
-					GraphicBoard[i][j].setOpaque(true);
+	private void UpdateGraphicBoard() {
+		this.repaint();
+		
+	}
+
+	@Override
+	protected void paintComponent(Graphics arg0) {
+		// TODO Auto-generated method stub
+		super.paintComponent(arg0);
+		
+		float w = this.getWidth()/realSize;
+		float h = this.getHeight()/realSize;
+		
+		for(int i = 0; i < realSize; i++) {
+			for(int j= 0; j < realSize; j++) {
+				if(g1.getBoard().getCurrentState()[j][i].equals(" ")) {
+					arg0.drawImage(Water, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
 				}
-				else if(g1.getBoard().getCurrentState()[i][j].equals("X")) {
-					GraphicBoard[i][j].setBackground(Color.DARK_GRAY);
-					GraphicBoard[i][j].setOpaque(false);
+				
+				else if(g1.getBoard().getCurrentState()[j][i].equals("X")) {
+					arg0.drawImage(Wall, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
 				}
 
-				else if (g1.getBoard().getCurrentState()[i][j].equals("E")) {
-					GraphicBoard[i][j].setBackground(Color.YELLOW);
+				else if (g1.getBoard().getCurrentState()[j][i].equals("E")) {
+					arg0.drawImage(sword, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50,50,null);
 				}
 
-				else if (g1.getBoard().getCurrentState()[i][j].equals("H") || g1.getBoard().getCurrentState()[i][j].equals("A")) {
-
-					GraphicBoard[i][j].setIcon(hero);
-					GraphicBoard[i][j].setOpaque(true);
+				else if (g1.getBoard().getCurrentState()[j][i].equals("H") || g1.getBoard().getCurrentState()[j][i].equals("A")) {
+					arg0.drawImage(Hero, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
 				}
 
-				else if (g1.getBoard().getCurrentState()[i][j].equals("D")) {
-					GraphicBoard[i][j].setBackground(Color.RED);
+				else if (g1.getBoard().getCurrentState()[j][i].equals("D")) {
+					arg0.drawImage(Dragon_awake, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
 				}
-
-				else if(g1.getBoard().getCurrentState()[i][j].equals("v") || g1.getBoard().getCurrentState()[i][j].equals("V")) {
-					GraphicBoard[i][j].setBackground(Color.GREEN);
+				
+				else if (g1.getBoard().getCurrentState()[j][i].equals("t")) {
+					arg0.drawImage(Dragon_sleeping, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
+				}
+				
+				else if(g1.getBoard().getCurrentState()[j][i].equals("v") || g1.getBoard().getCurrentState()[j][i].equals("V")) {
+					arg0.drawImage(Eagle, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
 				}
 			}
 		}
+		
+		
+
+
+
+
 	}
-
-
 
 }
 
