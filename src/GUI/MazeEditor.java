f5e36f8dcc1d5e32c7cf10ce7d9eefd75733dc59
ei12058 <ei12058@481db94f-f5f5-4b71-ab5e-8d926a57a53e>
2014-04-15 18:01:05 +0000

    mazeEditor v1.2

--- src/GUI/MazeEditor.java
+++ src/GUI/MazeEditor.java
@@ -27,14 +27,17 @@ public class MazeEditor extends JPanel {
 
 	private static final long serialVersionUID = 7836117138272018391L;
 
-	private BufferedImage sword = creatImage("imagens/sword.jpg");
 	private BufferedImage Wall =  creatImage("imagens/muralhaTemp.jpg");
 	private BufferedImage Water =  creatImage("imagens/Water.gif");
+	private BufferedImage sword = creatImage("imagens/sword.jpg");
 	private BufferedImage Hero =  creatImage("imagens/hero.jpg");
 	private BufferedImage Hero_sword =  creatImage("imagens/HeroSword.jpg");
 	private BufferedImage Dragon_awake  =  creatImage("imagens/dragonAwake.png");
 	private BufferedImage Dragon_sleeping = creatImage("images/DragonSleeping.jpg");
 	private BufferedImage Eagle  =  creatImage("imagens/eagle.jpg");
+
+	private String[] choose = {" ", "X", "H", "D", "E"};
+	private int choose1=0;
 
 
 	public MazeEditor(int size,MazeGameGUI window) {
@@ -54,13 +57,20 @@ public class MazeEditor extends JPanel {
 		this.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-
 				int n = (int) (e.getPoint().getX()/(frame.getWidth()/(realSize)));
 				int m = (int) (e.getPoint().getY()/(frame.getHeight()/(realSize+1)));
 
-				if(g1.getBoard().getCurrentState()[m][n].equals("X"))
-					g1.getBoard().changeCurrentMaze(m, n, " ");
-				else g1.getBoard().changeCurrentMaze(m, n, "X");
+				if(e.getButton() == MouseEvent.BUTTON1) {
+					if(g1.getBoard().getCurrentState()[m][n].equals("X"))
+						g1.getBoard().changeCurrentMaze(m, n, " ");
+					else g1.getBoard().changeCurrentMaze(m, n, "X");
+				}
+				else {
+					if(choose1>=4) choose1=0;
+					else choose1++;
+
+					g1.getBoard().changeCurrentMaze(m, n, choose[choose1]);
+				}
 
 				updateGraphicBoard();
 			}
@@ -95,8 +105,34 @@ public class MazeEditor extends JPanel {
 				else if(g1.getBoard().getCurrentState()[j][i].equals("X")) {
 					arg0.drawImage(Wall, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
 				}
+
+				else if (g1.getBoard().getCurrentState()[j][i].equals("E")) {
+					arg0.drawImage(sword, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50,50,null);
+				}
+
+				else if (g1.getBoard().getCurrentState()[j][i].equals("H")) {
+					arg0.drawImage(Hero, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
+				}
+
+				else if (g1.getBoard().getCurrentState()[j][i].equals("A")) {
+					arg0.drawImage(Hero_sword, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
+				}
+
+				else if (g1.getBoard().getCurrentState()[j][i].equals("D")) {
+					arg0.drawImage(Dragon_awake, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
+				}
+
+
+				else if(g1.getBoard().getCurrentState()[j][i].equals("v") || g1.getBoard().getCurrentState()[j][i].equals("V")) {
+					arg0.drawImage(Eagle, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
+				}
+
+				else {
+					arg0.drawImage(Dragon_sleeping, (int) (0+w*i), (int)(0+h*j), (int)(w+w*i),(int) (h+h*j), 0, 0, 50, 50, null);
+				}
 			}
 		}
+
 	}
 
 	private void updateGraphicBoard() {
