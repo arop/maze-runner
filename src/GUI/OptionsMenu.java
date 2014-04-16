0248678a4080409ca2583ff1136ad46db3992c28
Filipe Batista <ei12068@fe.up.pt>
2014-04-16 16:20:48 +0000

    OptionsMenu final

--- src/GUI/OptionsMenu.java
+++ src/GUI/OptionsMenu.java
@@ -26,6 +26,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.SpinnerNumberModel;
 import java.awt.Font;
 import java.awt.FlowLayout;
+import java.awt.Color;
 
 public class OptionsMenu extends JPanel {
 	
@@ -40,30 +41,45 @@ public class OptionsMenu extends JPanel {
 	private int numberDrags = 1;
 	
 	public OptionsMenu(Game currentGame,MazeGameGUI window) {
+		setForeground(Color.WHITE);
+		setBackground(Color.BLACK);
 		g1 = currentGame;
 		frame = window;
 		setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.BLACK);
+		panel.setForeground(Color.WHITE);
 		add(panel, BorderLayout.NORTH);
-		panel.setLayout(new GridLayout(2, 1, 0, 0));
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JLabel lblNewLabel_12 = new JLabel("");
+		panel.add(lblNewLabel_12);
 		
 		JLabel lblNewLabel_11 = new JLabel("OPTIONS MENU");
+		lblNewLabel_11.setForeground(Color.RED);
 		lblNewLabel_11.setFont(new Font("Trajan Pro", Font.PLAIN, 25));
 		lblNewLabel_11.setHorizontalAlignment(SwingConstants.CENTER);
 		panel.add(lblNewLabel_11);
+		
+		JLabel lblNewLabel_13 = new JLabel("");
+		panel.add(lblNewLabel_13);
 		
 		JPanel panel_1 = new JPanel();
 		add(panel_1, BorderLayout.SOUTH);
 		panel_1.setLayout(new GridLayout(4, 2, 0, 0));
 		
-		JLabel lblNewLabel = new JLabel("");
+		JPanel lblNewLabel = new JPanel();
+		lblNewLabel.setBackground(Color.BLACK);
 		panel_1.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("");
+		JPanel lblNewLabel_1 = new JPanel();
+		lblNewLabel_1.setBackground(Color.BLACK);
 		panel_1.add(lblNewLabel_1);
 		
 		JButton btnNewButton_1 = new JButton("Cancel");
+		btnNewButton_1.setForeground(Color.RED);
+		btnNewButton_1.setBackground(Color.BLACK);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame.disableAll();
@@ -72,6 +88,8 @@ public class OptionsMenu extends JPanel {
 		});
 		
 		JButton btnNewButton_2 = new JButton("Default");
+		btnNewButton_2.setForeground(Color.RED);
+		btnNewButton_2.setBackground(Color.BLACK);
 		panel_1.add(btnNewButton_2);
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -83,6 +101,8 @@ public class OptionsMenu extends JPanel {
 		});
 		
 		JButton btnNewButton_3 = new JButton("Custom");
+		btnNewButton_3.setForeground(Color.RED);
+		btnNewButton_3.setBackground(Color.BLACK);
 		panel_1.add(btnNewButton_3);
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -92,6 +112,8 @@ public class OptionsMenu extends JPanel {
 		});
 		
 		JButton btnNewButton = new JButton("Confirm");
+		btnNewButton.setForeground(Color.RED);
+		btnNewButton.setBackground(Color.BLACK);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				frame.disableAll();
@@ -104,10 +126,12 @@ public class OptionsMenu extends JPanel {
 		panel_1.add(btnNewButton);
 		panel_1.add(btnNewButton_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("");
+		JPanel lblNewLabel_2 = new JPanel();
+		lblNewLabel_2.setBackground(Color.BLACK);
 		panel_1.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("");
+		JPanel lblNewLabel_3 = new JPanel();
+		lblNewLabel_3.setBackground(Color.BLACK);
 		panel_1.add(lblNewLabel_3);
 		
 		JPanel panel_2 = new JPanel();
@@ -127,13 +151,19 @@ public class OptionsMenu extends JPanel {
 		panel_4.setLayout(new GridLayout(0, 1, 0, 0));
 		
 		JPanel panel_8 = new JPanel();
+		panel_8.setForeground(Color.BLACK);
+		panel_8.setBackground(Color.BLACK);
 		panel_4.add(panel_8);
 		panel_8.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JButton btnNewButton_5 = new JButton("Windowed");
+		btnNewButton_5.setForeground(Color.RED);
+		btnNewButton_5.setBackground(Color.BLACK);
 		panel_8.add(btnNewButton_5);
 		
 		JButton btnNewButton_4 = new JButton("Fullscreen");
+		btnNewButton_4.setForeground(Color.RED);
+		btnNewButton_4.setBackground(Color.BLACK);
 		panel_8.add(btnNewButton_4);
 		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -152,17 +182,22 @@ public class OptionsMenu extends JPanel {
 		});
 		
 		JPanel panel_9 = new JPanel();
+		panel_9.setBackground(Color.BLACK);
 		panel_4.add(panel_9);
 		
 		JLabel lblNewLabel_5 = new JLabel("Size of Maze");
+		lblNewLabel_5.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel_5.setForeground(Color.RED);
 		panel_9.add(lblNewLabel_5);
-		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.RIGHT);
 		
 		final JSpinner spinner = new JSpinner();
 		panel_9.add(spinner);
 		spinner.setModel(new SpinnerNumberModel(11, 7, 51, 2));
 		
 		JLabel lblNewLabel_4 = new JLabel("Number of Dragons");
+		lblNewLabel_4.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel_4.setForeground(Color.RED);
 		panel_9.add(lblNewLabel_4);
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
 		
@@ -185,10 +220,16 @@ public class OptionsMenu extends JPanel {
 		panel_7.setLayout(new GridLayout(1, 2, 0, 0));
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Sleeping Dragons");
+		chckbxNewCheckBox.setVerticalAlignment(SwingConstants.TOP);
+		chckbxNewCheckBox.setForeground(Color.RED);
+		chckbxNewCheckBox.setBackground(Color.BLACK);
 		chckbxNewCheckBox.setHorizontalAlignment(SwingConstants.RIGHT);
 		panel_7.add(chckbxNewCheckBox);
 		
 		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("Moving Dragons");
+		chckbxNewCheckBox_1.setVerticalAlignment(SwingConstants.TOP);
+		chckbxNewCheckBox_1.setForeground(Color.RED);
+		chckbxNewCheckBox_1.setBackground(Color.BLACK);
 		chckbxNewCheckBox_1.setHorizontalAlignment(SwingConstants.LEFT);
 		panel_7.add(chckbxNewCheckBox_1);
 		chckbxNewCheckBox_1.addActionListener(new ActionListener() {
@@ -203,19 +244,34 @@ public class OptionsMenu extends JPanel {
 		});
 		
 		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(Color.BLACK);
 		panel_4.add(panel_5);
 		panel_5.setLayout(new GridLayout(2, 5, 0, 0));
 		
 		JLabel lblNewLabel_6 = new JLabel("RIGHT");
+		lblNewLabel_6.setForeground(Color.RED);
+		lblNewLabel_6.setBackground(Color.BLACK);
+		lblNewLabel_6.setOpaque(true);
 		panel_5.add(lblNewLabel_6);
 		
 		JLabel lblNewLabel_7 = new JLabel("UP");
+		lblNewLabel_7.setForeground(Color.RED);
+		lblNewLabel_7.setBackground(Color.BLACK);
+		lblNewLabel_7.setOpaque(true);
 		panel_5.add(lblNewLabel_7);
 		
 		JLabel lblNewLabel_8 = new JLabel("LEFT");
+		lblNewLabel_8.setForeground(Color.RED);
+		lblNewLabel_8.setBackground(Color.BLACK);
+		lblNewLabel_8.setOpaque(true);
+
 		panel_5.add(lblNewLabel_8);
 		
 		JLabel lblNewLabel_9 = new JLabel("DOWN");
+		lblNewLabel_9.setForeground(Color.RED);
+		lblNewLabel_9.setBackground(Color.BLACK);
+		lblNewLabel_9.setOpaque(true);
+
 		lblNewLabel_9.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
@@ -225,9 +281,14 @@ public class OptionsMenu extends JPanel {
 		panel_5.add(lblNewLabel_9);
 		
 		JLabel lblNewLabel_10 = new JLabel("Eagle");
+		lblNewLabel_10.setForeground(Color.RED);
+		lblNewLabel_10.setBackground(Color.BLACK);
+		lblNewLabel_10.setOpaque(true);
+
 		panel_5.add(lblNewLabel_10);
 		
 		textField_1 = new JTextField();
+		textField_1.setBackground(Color.DARK_GRAY);
 		textField_1.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent arg0) {
@@ -239,6 +300,7 @@ public class OptionsMenu extends JPanel {
 		textField_1.setColumns(10);
 		
 		textField = new JTextField();
+		textField.setBackground(Color.DARK_GRAY);
 		textField.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent arg0) {
@@ -249,6 +311,7 @@ public class OptionsMenu extends JPanel {
 		textField.setColumns(10);
 		
 		textField_2 = new JTextField();
+		textField_2.setBackground(Color.DARK_GRAY);
 		textField_2.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent arg0) {
@@ -259,10 +322,12 @@ public class OptionsMenu extends JPanel {
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
+		textField_3.setBackground(Color.DARK_GRAY);
 		panel_5.add(textField_3);
 		textField_3.setColumns(10);
 		
 		textField_4 = new JTextField();
+		textField_4.setBackground(Color.DARK_GRAY);
 		panel_5.add(textField_4);
 		textField_4.setColumns(10);
 	}
